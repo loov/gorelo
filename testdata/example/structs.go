f436da3058f899c6f1b1c613b2e3436657703db7
Egon Elbre <egonelbre@gmail.com>
2026-03-17 01:17:16 +0200

    testdata/example: add using anonymous structs

--- testdata/example/structs.go
+++ testdata/example/structs.go
@@ -30,3 +30,16 @@ type Server struct {
 		KeyFile  string
 	}
 }
+
+// Method on Server struct using anonymous fields.
+func (server *Server) Print() {
+	fmt.Println(server.Addr)
+	fmt.Println(server.TLS.CertFile)
+	fmt.Println(server.TLS.KeyFile)
+}
+
+// Function on package level using anonymous fields.
+func PrintConfig() {
+	fmt.Println(Config.Host)
+	fmt.Println(Config.Port)
+}
